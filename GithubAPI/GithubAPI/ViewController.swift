94423c57693dd3d1eb9fd769d050190e33e8076a
sajacl <sajaclvishkai@gmail.com>
2023-01-05 00:32:41 +0100

    Create project with XCode 14.2.
    #Evolution:
    
    Main reason for choosing XCode 14.2 is: This is the only version that simulators works better (on m1).

---
+++ GithubAPI/GithubAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubAPI
+//
+//  Created by Sajad Vishkai on 2023-01-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
